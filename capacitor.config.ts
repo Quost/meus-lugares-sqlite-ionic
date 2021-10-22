974b68c25021176380fc685a5521cc2da3784f98
Matheus Quost <matheusquost@gmail.com>
2021-10-21 23:39:33 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'A06-meus-lugares',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
